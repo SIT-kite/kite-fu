6d5dbc51fbe3d2694e59791f0fce28abc59f9f77
Zhangzqs <i@zhangzqs.cn>
2022-01-27 23:06:09 +0800

    Add: 移植登录页

---
+++ lib/page/login.dart
@@ -0,0 +1,257 @@
+import 'package:flash/flash.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:kite_fu/global/session_pool.dart';
+import 'package:kite_fu/global/storage_pool.dart';
+import 'package:kite_fu/util/flash.dart';
+import 'package:kite_fu/util/url_launcher.dart';
+import 'package:kite_fu/util/validation.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  // Text field controllers.
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _proxyInputController = TextEditingController();
+
+  final GlobalKey _formKey = GlobalKey<FormState>();
+
+  final TapGestureRecognizer _recognizer = TapGestureRecognizer()..onTap = onOpenUserLicense;
+
+  // State
+  bool isPasswordClear = false;
+  bool isLicenseAccepted = false;
+  bool isProxySettingShown = false;
+
+  /// 用户点击登录按钮后
+  Future<void> onLogin() async {
+    bool formValid = (_formKey.currentState as FormState).validate();
+    if (!formValid) {
+      return;
+    }
+    if (!isLicenseAccepted) {
+      showBasicFlash(context, const Text('请勾选用户协议'));
+      return;
+    }
+
+    final auth = AuthItem()
+      ..username = _usernameController.text
+      ..password = _passwordController.text;
+    try {
+      await SessionPool.ssoSession.login(auth.username, auth.password);
+    } on CredentialsInvalidException catch (e) {
+      showBasicFlash(context, Text(e.msg));
+      return;
+    } catch (e) {
+      showBasicFlash(context, Text('未知错误: ' + e.toString()));
+      return;
+    }
+
+    StoragePool.authPool.add(auth);
+    StoragePool.authSetting.currentUsername = auth.username;
+    Navigator.pushReplacementNamed(context, '/home');
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    String? username = StoragePool.authSetting.currentUsername;
+    if (username != null) {
+      _usernameController.text = username;
+      _passwordController.text = StoragePool.authPool.get(username)!.password;
+    }
+  }
+
+  static void onOpenUserLicense() {}
+
+  Widget buildTitleLine() {
+    return Container(
+        alignment: Alignment.centerLeft,
+        child: const Text('欢迎登录', style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold)));
+  }
+
+  Widget buildLoginForm() {
+    return Form(
+      autovalidateMode: AutovalidateMode.always,
+      key: _formKey,
+      child: Column(
+        children: [
+          TextFormField(
+            controller: _usernameController,
+            autofocus: true,
+            decoration: const InputDecoration(labelText: '学号', hintText: '输入你的学号', icon: Icon(Icons.person)),
+            validator: studentIdValidator,
+          ),
+          TextFormField(
+            controller: _passwordController,
+            autofocus: true,
+            obscureText: !isPasswordClear,
+            decoration: InputDecoration(
+              suffixIcon: IconButton(
+                icon: Icon(isPasswordClear ? Icons.visibility_off : Icons.visibility),
+                onPressed: () {
+                  setState(() {
+                    isPasswordClear = !isPasswordClear;
+                  });
+                },
+              ),
+              labelText: '密码',
+              hintText: '输入你的密码',
+              icon: const Icon(Icons.lock),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget buildUserLicenseCheckbox() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Checkbox(
+          value: isLicenseAccepted,
+          onChanged: (_isLicenseAccepted) {
+            setState(() => isLicenseAccepted = _isLicenseAccepted!);
+          },
+        ),
+        Text.rich(
+          TextSpan(
+            children: [
+              const TextSpan(text: '我已阅读并同意'),
+              TextSpan(text: '《上应小风筝用户协议》', style: const TextStyle(color: Colors.blue), recognizer: _recognizer),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget buildLoginButton() {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        SizedBox(
+          height: 40,
+          child: ElevatedButton(
+            onPressed: onLogin,
+            child: const Text('进入风筝元宇宙'),
+          ),
+        ),
+        TextButton(
+          child: const Text(
+            '忘记密码?',
+            style: TextStyle(color: Colors.grey),
+          ),
+          onPressed: () {
+            launchInBrowser(
+                'https://authserver.sit.edu.cn/authserver/getBackPasswordMainPage.do?service=https%3A%2F%2Fmyportal.sit.edu.cn%3A443%2F');
+          },
+        ),
+      ],
+    );
+  }
+
+  Widget _buildProxySetButton(BuildContext context, FlashController<dynamic> controller, _) {
+    return IconButton(
+      onPressed: () {
+        final String inputText = _proxyInputController.text;
+
+        if (proxyValidator(inputText) != null) {
+          return;
+        }
+        controller.dismiss();
+        isProxySettingShown = false;
+
+        StoragePool.network.useProxy = true;
+        StoragePool.network.proxy = inputText;
+        SessionPool.init();
+      },
+      icon: const Icon(Icons.send),
+    );
+  }
+
+  void _showProxyInput() {
+    if (isProxySettingShown) {
+      return;
+    }
+    isProxySettingShown = true;
+    _proxyInputController.text = StoragePool.network.proxy;
+
+    context.showFlashBar(
+      persistent: true,
+      borderWidth: 3,
+      behavior: FlashBehavior.fixed,
+      forwardAnimationCurve: Curves.fastLinearToSlowEaseIn,
+      title: const Text('设置代理服务'),
+      content: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text('格式如 192.168.1.1:8000'),
+          Form(
+            child: TextFormField(
+              controller: _proxyInputController,
+              validator: proxyValidator,
+              autofocus: true,
+            ),
+          ),
+        ],
+      ),
+      primaryActionBuilder: _buildProxySetButton,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var screenWidth = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      body: Stack(
+        children: [
+          // Proxy setting
+          Positioned(
+            top: 40,
+            right: 10,
+            child: IconButton(
+              icon: const Icon(Icons.settings, size: 35),
+              onPressed: () => _showProxyInput(),
+            ),
+          ),
+          Center(
+            child:
+                // Create new container and make it center in vertical direction.
+                Container(
+              width: screenWidth,
+              padding: const EdgeInsets.fromLTRB(50, 0, 50, 0),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  // Title field.
+                  buildTitleLine(),
+                  const Padding(padding: EdgeInsets.only(top: 40.0)),
+                  // Form field: username and password.
+                  buildLoginForm(),
+                  const SizedBox(height: 10),
+                  // User license check box.
+                  buildUserLicenseCheckbox(),
+                  const SizedBox(height: 25),
+                  // Login button.
+                  buildLoginButton(),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
