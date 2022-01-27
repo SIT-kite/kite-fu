843b048871a13073ece6f14ec8c3de5cd45bfe2b
Zhangzqs <i@zhangzqs.cn>
2022-01-27 22:56:49 +0800

    Add: 添加ServicePool

--- lib/global/service_pool.dart
+++ lib/global/service_pool.dart
@@ -1,1 +1,10 @@
-class ServicePool {}
+import 'package:kite_fu/dao/fu.dart';
+import 'package:kite_fu/global/session_pool.dart';
+import 'package:kite_fu/service/kite/fu.dart';
+
+class ServicePool {
+  static late FuDao fu;
+  void init() {
+    fu = FuService(SessionPool.kiteSession);
+  }
+}
