c542db73c93a72d9e9061a4fb611122d331955da
Zhangzqs <i@zhangzqs.cn>
2022-01-28 19:00:41 +0800

    Update: 完善扫福的业务逻辑

--- lib/service/fu.dart
+++ lib/service/fu.dart
@@ -3,6 +3,7 @@ import 'dart:typed_data';
 import 'package:kite_fu/dao/fu.dart';
 import 'package:kite_fu/entity/fu.dart';
 import 'package:kite_fu/session/abstract_session.dart';
+import 'package:kite_fu/util/logger.dart';
 
 import 'abstract_service.dart';
 
@@ -12,19 +13,24 @@ class FuService extends AService implements FuDao {
   @override
   Future<List<MyCard>> getList() async {
     final response = await session.get('/badge/card');
-    print(response.data);
-    return [];
+    List<dynamic> list = response.data;
+    return list.map((e) => MyCard.fromJson(e)).toList();
   }
 
   @override
   Future<PraiseResult> getResult() async {
-    // TODO: implement getResult
-    throw UnimplementedError();
+    final response = await session.get('/badge/result');
+    return PraiseResult.fromJson(response.data);
   }
 
   @override
   Future<UploadResultModel> upload(Uint8List imageBuffer) async {
-    // TODO: implement upload
-    throw UnimplementedError();
+    final response = await session.post(
+      '/badge/image',
+      data: imageBuffer,
+      contentType: 'multipart/form-data',
+    );
+    Log.info(response);
+    return UploadResultModel();
   }
 }
