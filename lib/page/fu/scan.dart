444ef14f07b45aa71ebae576d25a5db16a53028b
Zhangzqs <i@zhangzqs.cn>
2022-01-28 19:03:53 +0800

    Update: 完善扫描页面的摄像头调用

--- lib/page/fu/scan.dart
+++ lib/page/fu/scan.dart
@@ -1,21 +1,68 @@
+import 'dart:convert';
 import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
-import 'package:kite_fu/global/mock_pool.dart';
+import 'package:flutter/services.dart';
+import 'package:kite_fu/global/service_pool.dart';
+import 'package:webviewx/webviewx.dart';
 
-class ScanPage extends StatelessWidget {
+class ScanPage extends StatefulWidget {
   const ScanPage({Key? key}) : super(key: key);
+
+  @override
+  State<ScanPage> createState() => _ScanPageState();
+}
+
+class _ScanPageState extends State<ScanPage> {
+  Size get screenSize => MediaQuery.of(context).size;
+  WebViewXController? webviewController;
+
+  Future<Uint8List?> takePhoto() async {
+    if (webviewController == null) {
+      return null;
+    } else {
+      String imageBase64 = await webviewController!.callJsMethod('takePhoto', []);
+      return base64Decode(imageBase64.split(',')[1]);
+    }
+  }
+
+  Widget buildCameraView(String htmlSource) {
+    return WebViewX(
+      initialContent: htmlSource,
+      initialSourceType: SourceType.html,
+      height: screenSize.height,
+      width: screenSize.width,
+      onWebViewCreated: (controller) => webviewController = controller,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('扫描页'),
+        title: const Text('扫描页'),
+        actions: [
+          IconButton(
+            onPressed: () async {
+              final image = await takePhoto();
+              await ServicePool.fu.upload(image!);
+            },
+            icon: const Icon(Icons.add),
+          ),
+        ],
       ),
-      body: ElevatedButton(
-        child: Text('点击上传图片'),
-        onPressed: () {
-          MockPool.fu.upload(Uint8List(0));
+      body: FutureBuilder<String>(
+        future: rootBundle.loadString('assets/scan/index.html'),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            if (snapshot.hasData) {
+              String data = snapshot.data!;
+              return buildCameraView(data);
+            }
+          }
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
         },
       ),
     );
