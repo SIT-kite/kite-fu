6166a8149b508c2a5fdc70303277118ed7b25da4
Zhangzqs <i@zhangzqs.cn>
2022-01-29 17:36:54 +0800

    Add: 添加福的原始素材

---
+++ other/福卡原图/png转jpg压缩脚本.py
@@ -0,0 +1,15 @@
+import cv2
+
+def convert(filename: str):
+    img = cv2.imread(filename)
+
+    h,w,_ = img.shape
+    print(w,h)
+    img = cv2.resize(img,(int(w/10),int(h/10)))
+
+    cv2.imwrite(filename.split('.')[0]+'.jpg',img)
+
+import os
+
+for filename in filter(lambda x:'.png' in x,os.listdir()):
+    convert(filename)
