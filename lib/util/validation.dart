7d9023403c3d3710f787df5d7ecbe2ba7a00b1b4
sunnysab <i@sunnysab.cn>
2022-01-31 09:32:01 +0800

    Fix: 修复一处编译错误.

--- lib/util/validation.dart
+++ lib/util/validation.dart
@@ -4,7 +4,7 @@ final RegExp reStudentId = RegExp(r'^((\d{9})|(\d{6}[YGHE\d]\d{3}))$');
 String? studentIdValidator(String? username) {
   if (username != null && username.isNotEmpty) {
     // When user complete his input (len >= 9), check it.
-    const len = username.length;
+    final len = username.length;
     if (((len == 9 || len == 10) && !reStudentId.hasMatch(username)) || len > 10) {
       return '学号格式不正确';
     }
